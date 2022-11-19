9a15ac786028f6f795212e60930f434eaf125bac
Fulvius Titanero Guelfi <fulvius@gpmail.com.br>
2022-11-19 17:14:32 -0300

    Commit from TeleportHQ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,58 +7,9 @@ import { AppComponent } from './app.component'
 
 const routes = [
   {
-    path: 'base-page',
-    loadChildren: () =>
-      import('./pages/base-page/base-page.module').then(
-        (m) => m.BasePageModule
-      ),
-  },
-  {
-    path: 'create-account',
-    loadChildren: () =>
-      import('./pages/create-account/create-account.module').then(
-        (m) => m.CreateAccountModule
-      ),
-  },
-  {
-    path: 'sing-in-base-page',
-    loadChildren: () =>
-      import('./pages/sing-in-base-page/sing-in-base-page.module').then(
-        (m) => m.SingInBasePageModule
-      ),
-  },
-  {
-    path: 'sessions-minimal-ligth',
-    loadChildren: () =>
-      import(
-        './pages/sessions-minimal-ligth/sessions-minimal-ligth.module'
-      ).then((m) => m.SessionsMinimalLigthModule),
-  },
-  {
     path: 'sing-up-w',
     loadChildren: () =>
       import('./pages/sing-up-w/sing-up-w.module').then((m) => m.SingUpWModule),
-  },
-  {
-    path: 'forgot-password',
-    loadChildren: () =>
-      import('./pages/forgot-password/forgot-password.module').then(
-        (m) => m.ForgotPasswordModule
-      ),
-  },
-  {
-    path: 'sing-in-widget1',
-    loadChildren: () =>
-      import('./pages/sing-in-widget1/sing-in-widget1.module').then(
-        (m) => m.SingInWidget1Module
-      ),
-  },
-  {
-    path: 'sing-up-minimal-ligth',
-    loadChildren: () =>
-      import('./pages/sing-up-minimal-ligth/sing-up-minimal-ligth.module').then(
-        (m) => m.SingUpMinimalLigthModule
-      ),
   },
   {
     path: 'sessions-w',
@@ -73,35 +24,9 @@ const routes = [
       import('./pages/sing-in-w/sing-in-w.module').then((m) => m.SingInWModule),
   },
   {
-    path: 'base-minimal-ligth',
-    loadChildren: () =>
-      import('./pages/base-minimal-ligth/base-minimal-ligth.module').then(
-        (m) => m.BaseMinimalLigthModule
-      ),
-  },
-  {
-    path: 'sing-in-minimal-ligth',
-    loadChildren: () =>
-      import('./pages/sing-in-minimal-ligth/sing-in-minimal-ligth.module').then(
-        (m) => m.SingInMinimalLigthModule
-      ),
-  },
-  {
     path: '',
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomeModule),
-  },
-  {
-    path: 'sing-in1',
-    loadChildren: () =>
-      import('./pages/sing-in1/sing-in1.module').then((m) => m.SingIn1Module),
-  },
-  {
-    path: 'sing-up-base-page',
-    loadChildren: () =>
-      import('./pages/sing-up-base-page/sing-up-base-page.module').then(
-        (m) => m.SingUpBasePageModule
-      ),
   },
 ]
 
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,49 +2,12 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { CommonModule } from '@angular/common'
 
-import { SingIn } from './sing-in/sing-in.component'
-import { Component1 } from './component1/component1.component'
-import { SingUpBase } from './sing-up-base/sing-up-base.component'
-import { AppComponent } from './component/component.component'
-import { SingInWidget } from './sing-in-widget/sing-in-widget.component'
-import { Component2 } from './component2/component2.component'
-import { TopBarBase } from './top-bar-base/top-bar-base.component'
 import { Chat } from './chat/chat.component'
-import { TopBar } from './top-bar/top-bar.component'
-import { SingInBase } from './sing-in-base/sing-in-base.component'
-import { SingUp } from './sing-up/sing-up.component'
-import { Forgot } from './forgot/forgot.component'
 
 @NgModule({
-  declarations: [
-    SingIn,
-    Component1,
-    SingUpBase,
-    AppComponent,
-    SingInWidget,
-    Component2,
-    TopBarBase,
-    Chat,
-    TopBar,
-    SingInBase,
-    SingUp,
-    Forgot,
-  ],
+  declarations: [Chat],
   imports: [CommonModule, RouterModule],
-  exports: [
-    SingIn,
-    Component1,
-    SingUpBase,
-    AppComponent,
-    SingInWidget,
-    Component2,
-    TopBarBase,
-    Chat,
-    TopBar,
-    SingInBase,
-    SingUp,
-    Forgot,
-  ],
+  exports: [Chat],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ComponentsModule {}
