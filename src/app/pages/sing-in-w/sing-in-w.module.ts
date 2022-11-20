280a0338e023184f33964a1b2feacbb8b40fc811
fulviusguelfi <fulvius@gpmail.com.br>
2022-11-20 02:41:31 -0300

    all jobs

--- src/app/pages/sing-in-w/sing-in-w.module.ts
+++ src/app/pages/sing-in-w/sing-in-w.module.ts
@@ -1,9 +1,10 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
-import { RouterModule } from '@angular/router'
-import { CommonModule } from '@angular/common'
+
 
 import { ComponentsModule } from '../../components/components.module'
 import { SingInW } from './sing-in-w.component'
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 const routes = [
   {
@@ -18,4 +19,4 @@ const routes = [
   exports: [SingInW],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class SingInWModule {}
+export class SingInWModule { }
